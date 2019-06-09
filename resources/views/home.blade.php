4c7413993ba12359c53330ce309f460509f5ff44
sebalopezz <jorgesebastianlopez@gmail.com>
2019-06-08 21:32:00 -0300

    card home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">¿En qué estas pensando ahora?</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +14,32 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form action="">
+                        <div class="form-group">
+                            <label for="thought"> Ahora estoy pensando en:</label>
+                            <input type="text" class="form-control" name="thought">
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            Enviar pensamiento    
+                        </button>
+                    </form>
                 </div>
             </div>
+            <div class="card">
+                    <div class="card-header">Publicado en 08/06/2019</div>
+    
+                    <div class="card-body">
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis expedita eos voluptatibus! Vitae in vel nostrum eum dolores officiis non, magni consequuntur nisi sapiente error voluptate magnam, iusto sed culpa. </p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-default">
+                            Editar
+                        </button>
+                        <button class="btn btn-danger">
+                            Eliminar
+                        </button>
+                    </div>
+                </div>
         </div>
     </div>
 </div>
